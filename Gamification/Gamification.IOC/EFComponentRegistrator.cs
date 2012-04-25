4bca7a42f3b7aaff03c14bde3fbf30356d8f4a9d
b1ff <Evgeniy@Dell.(none)>
2012-04-25 20:24:54 +0300

    Added services project, fixed IOC component registration.

--- Gamification/Gamification.IOC/EFComponentRegistrator.cs
+++ Gamification/Gamification.IOC/EFComponentRegistrator.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web;
+using Castle.Core;
 using Castle.Facilities.WcfIntegration;
 using Castle.MicroKernel.Registration;
 using Castle.Windsor;
@@ -14,28 +15,24 @@ namespace Gamification.IOC
         {
             var container = new WindsorContainer();
 
-            container.Register(
-                Component.For<IApplicationSettings>().ImplementedBy<FromConfigFileConfiguration>().LifeStyle.Singleton);
-            container.Register(Component.For<EfDbContext>().UsingFactoryMethod(x =>
-                                                                                   {
-                                                                                       var appSettings = x.Resolve<IApplicationSettings>();
-                                                                                       return new EfDbContext(appSettings.ConnectionString);
-                                                                                   }).LifeStyle.Transient);
-            container.Register(
-                    AllTypes.FromAssembly(typeof(EfRepository<>).Assembly)
-                        .Pick()
-                        .WithService.DefaultInterfaces()
-                        .LifestyleTransient());
-
+            container.Register(Component.For<IApplicationSettings>().ImplementedBy<FromConfigFileConfiguration>().LifeStyle.Singleton);
             container.Register(Component.For<IWindsorContainer>().Instance(container));
+
             return container;
         }
 
         public static IWindsorContainer GetWebContainer()
         {
             var container = BaseGetContainer();
-            container.Register(
-                Component.For<HttpContextBase>().UsingFactoryMethod(x => new HttpContextWrapper(HttpContext.Current)).LifeStyle.PerWebRequest);
+            container.Register(Component.For<EfDbContext>().UsingFactoryMethod(x =>
+            {
+                var appSettings = x.Resolve<IApplicationSettings>();
+                return new EfDbContext(appSettings.ConnectionString);
+            }).LifeStyle.PerWebRequest);
+
+            container.Register(AllTypes.FromAssembly(typeof(EfRepository<>).Assembly).Pick().WithService.DefaultInterfaces().LifestylePerWebRequest());
+            container.Register(Component.For<HttpContextBase>().UsingFactoryMethod(x => new HttpContextWrapper(HttpContext.Current)).LifeStyle.PerWebRequest);
+
             return container;
         }
 
@@ -43,6 +40,15 @@ namespace Gamification.IOC
         {
             var container = BaseGetContainer();
             container.AddFacility<WcfFacility>();
+            container.Register(Component.For<EfDbContext>().UsingFactoryMethod(x =>
+            {
+                var appSettings = x.Resolve<IApplicationSettings>();
+                return new EfDbContext(appSettings.ConnectionString);
+            }).LifeStyle.PerWcfOperation());
+
+            container.Register(AllTypes.FromAssembly(typeof(EfRepository<>).Assembly).Pick().WithService.DefaultInterfaces().LifestylePerWcfOperation());
+            container.Register(Component.For<HttpContextBase>().UsingFactoryMethod(x => new HttpContextWrapper(HttpContext.Current)).LifeStyle.PerWcfOperation());
+
             return container;
         }
     }
